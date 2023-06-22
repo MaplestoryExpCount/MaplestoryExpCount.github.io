88a14d0c4dfdf6c340adbd85662f62e76419198a
MaplestoryExpCount <56793641+MaplestoryExpCount@users.noreply.github.com>
2023-06-22 13:20:24 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,4 +1,4 @@
-webtitle = 'V253&nbsp; Savior&nbsp; 版本&nbsp;經驗成長秘藥計算機 Beta 1.0.7'
+webtitle = '新楓之谷&nbsp;V253&nbsp; Savior&nbsp; 版本&nbsp;經驗成長秘藥計算機 Beta 1.0.7'
 
 $('.webtitle').html(webtitle);
 document.title = $('.webtitle').text();
